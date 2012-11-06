2b6f29d8b3f47308f0179c6200880c2bdf356518
Andrew Duthie <aduthie7@gmail.com>
2012-11-05 22:26:03 -0500

    Initial commit

---
+++ js/ie.js
@@ -0,0 +1,143 @@
+/*
+  localStorage Polyfill
+  Author: Remy Sharp
+  https://gist.github.com/350433
+*/
+if (typeof window.localStorage == 'undefined' || typeof window.sessionStorage == 'undefined') (function () {
+
+var Storage = function (type) {
+  function createCookie(name, value, days) {
+    var date, expires;
+
+    if (days) {
+      date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toGMTString();
+    } else {
+      expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  function readCookie(name) {
+    var nameEQ = name + "=",
+        ca = document.cookie.split(';'),
+        i, c;
+
+    for (i=0; i < ca.length; i++) {
+      c = ca[i];
+      while (c.charAt(0)==' ') {
+        c = c.substring(1,c.length);
+      }
+
+      if (c.indexOf(nameEQ) == 0) {
+        return c.substring(nameEQ.length,c.length);
+      }
+    }
+    return null;
+  }
+
+  function setData(data) {
+    data = JSON.stringify(data);
+    if (type == 'session') {
+      window.name = data;
+    } else {
+      createCookie('localStorage', data, 365);
+    }
+  }
+
+  function clearData() {
+    if (type == 'session') {
+      window.name = '';
+    } else {
+      createCookie('localStorage', '', 365);
+    }
+  }
+
+  function getData() {
+    var data = type == 'session' ? window.name : readCookie('localStorage');
+    return data ? JSON.parse(data) : {};
+  }
+
+
+  // initialise if there's already data
+  var data = getData();
+
+  return {
+    length: 0,
+    clear: function () {
+      data = {};
+      this.length = 0;
+      clearData();
+    },
+    getItem: function (key) {
+      return data[key] === undefined ? null : data[key];
+    },
+    key: function (i) {
+      // not perfect, but works
+      var ctr = 0;
+      for (var k in data) {
+        if (ctr == i) return k;
+        else ctr++;
+      }
+      return null;
+    },
+    removeItem: function (key) {
+      delete data[key];
+      this.length--;
+      setData(data);
+    },
+    setItem: function (key, value) {
+      data[key] = value+''; // forces the value to a string
+      this.length++;
+      setData(data);
+    }
+  };
+};
+
+if (typeof window.localStorage == 'undefined') window.localStorage = new Storage('local');
+if (typeof window.sessionStorage == 'undefined') window.sessionStorage = new Storage('session');
+
+})();
+
+/**
+ * Date.parse with progressive enhancement for ISO 8601 <https://github.com/csnover/js-iso8601>
+ * © 2011 Colin Snover <http://zetafleet.com>
+ * Released under MIT license.
+ */
+(function (Date, undefined) {
+    var origParse = Date.parse, numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
+    Date.parse = function (date) {
+        var timestamp, struct, minutesOffset = 0;
+
+        // ES5 §15.9.4.2 states that the string should attempt to be parsed as a Date Time String Format string
+        // before falling back to any implementation-specific date parsing, so that’s what we do, even if native
+        // implementations could be faster
+        //              1 YYYY                2 MM       3 DD           4 HH    5 mm       6 ss        7 msec        8 Z 9 ±    10 tzHH    11 tzmm
+        if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(date))) {
+            // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
+            for (var i = 0, k; (k = numericKeys[i]); ++i) {
+                struct[k] = +struct[k] || 0;
+            }
+
+            // allow undefined days and months
+            struct[2] = (+struct[2] || 1) - 1;
+            struct[3] = +struct[3] || 1;
+
+            if (struct[8] !== 'Z' && struct[9] !== undefined) {
+                minutesOffset = struct[10] * 60 + struct[11];
+
+                if (struct[9] === '+') {
+                    minutesOffset = 0 - minutesOffset;
+                }
+            }
+
+            timestamp = Date.UTC(struct[1], struct[2], struct[3], struct[4], struct[5] + minutesOffset, struct[6], struct[7]);
+        }
+        else {
+            timestamp = origParse ? origParse(date) : NaN;
+        }
+
+        return timestamp;
+    };
+}(Date));
